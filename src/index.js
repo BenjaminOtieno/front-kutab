99158e81a4020542a9436cc238f4d04d6e4a9aad
BenjaminOtieno <benjamin.otieno@moringaschool.com>
2022-12-09 15:25:12 +0300

    index.js added

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import{ Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import rootReducer from './reducers/reducer.js';
+
+const store = createStore(rootReducer, compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f
+  ));
+
+ReactDOM.render(
+<Provider store={store}>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+</Provider>, document.getElementById('root'));
+
