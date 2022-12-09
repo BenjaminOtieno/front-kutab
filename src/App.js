5fb561a0622905fe4bc44400d48aea5e2834725b
BenjaminOtieno <benjamin.otieno@moringaschool.com>
2022-12-09 15:23:16 +0300

    app.js added

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import './App.css';
+
+
+import BooksContainer from './containers/BooksContainer';
+import UsersContainer from './containers/UsersContainer';
+import NavBar from './NavBar';
+import Welcome from './components/users/Welcome';
+
+import { fetchUser } from './actions/fetchUser'
+
+
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchUser()
+  }
+
+  render() {
+
+    return (
+      <div className="App" >
+        <NavBar userId={this.props.userId} />
+
+        <Switch>
+          <Route path='/books' render={(routerProps) => <BooksContainer {...routerProps} />} />
+          <Route path='/users/:id/books' component={UsersContainer} />
+          <Route exact path='/' component={Welcome} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+const mapState = state => {
+  return {
+    userId: state.user.id
+  }
+}
+
+
+export default connect(mapState, { fetchUser })(App)
